9f148c4d9974bdb955f1bdcaacaac1070bb54812
Arpitaa <arpitaatigre@gmail.com>
2025-06-13 11:35:12 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,27 @@ import CoursesPage from "./pages/CoursesPage";
 import AdmissionsPage from "./pages/AdmissionsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
+import { useState } from "react";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Arpita Sitaram Atigre"
+          studentPhotoUrl="/images/my-image2.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+      
     <Router>
       <div className="main-layout">
         <Header />
@@ -30,6 +48,7 @@ function App() {
         <ChatbotComponent/>
       </div>
     </Router>
+    </>
   );
 }
 export default App;
